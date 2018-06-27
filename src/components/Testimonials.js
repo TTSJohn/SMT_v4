e5a893affa31ee139a785e71be7d7eb008d4d53c
John Moranski <johnnymoranski21@gmail.com>
2018-06-27 11:53:41 -0500

    fixed carousel display

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,21 +8,21 @@ class Testimonials extends Component {
 			<Grid>
 				<Carousel>
 				  <Carousel.Item>
-				    <Image width={900} height={500} alt="900x500" src="assets/massagePhoto4.jpg" />
+				    <Image width={2000} height={500} alt="2000x500" src="assets/massagePhoto4.jpg" />
 				    <Carousel.Caption className="editText">
 				      <h3 className="slideOne">First slide label</h3>
 				      <p className="slideOne">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
 				    </Carousel.Caption>
 				  </Carousel.Item>
 				  <Carousel.Item>
-				    <Image width={900} height={500} alt="900x500" src="assets/massagePhoto2.jpeg" />
+				    <Image width={2000} height={500} alt="2000x500" src="assets/massagePhoto2.jpeg" />
 				    <Carousel.Caption className="editText">
 				      <h3>Second slide label</h3>
 				      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				    </Carousel.Caption>
 				  </Carousel.Item>
 				  <Carousel.Item>
-				    <Image width={900} height={500} alt="900x500" src="assets/massagePhoto3.jpeg" />
+				    <Image width={2000} height={200} alt="2000x500" src="assets/massagePhoto3.jpeg" />
 				    <Carousel.Caption className="editText">
 				      <h3>Third slide label</h3>
 				      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
